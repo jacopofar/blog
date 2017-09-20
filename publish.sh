8476803f23c45ced25c02fe0eb00b9237e620680
jacopofar <jacopofar@users.noreply.github.com>
2017-09-20 15:32:00 +0200

    Initial setup

---
+++ publish.sh
@@ -0,0 +1,15 @@
+# remove previous publication
+rm -rf public
+mkdir public
+
+# clone gh-pages branch from the local repo into a repo located within "public in the repo folder itself"
+git clone .git --branch gh-pages public
+  
+# generate
+hugo
+  
+# commit the changes in the clone and push them back to the local gh-pages branch
+read PUBLISH_MESSAGE && cd public && git add --all && git commit -m $PUBLISH_MESSAGE  && git push origin gh-pages
+
+# publish
+git push upstream gh-pages
